c0a825e4dd3825bc0f6d50ae514a218fedb942f1
gojutin <gojutin@gmail.com>
2020-03-31 21:45:35 -0400

    Updated WiredButton test.

--- test/WiredButton.test.tsx
+++ test/WiredButton.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/WiredButton.stories';
+import { WiredButton } from '../src/WiredButton';
 
-describe('Thing', () => {
+describe('WiredButton', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<WiredButton />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
